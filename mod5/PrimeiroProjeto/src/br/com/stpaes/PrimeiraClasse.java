8d8264d311d688a76d8bfb3f6a4b90e322a89ccd
stpaes <steefaaniepr@outlook.com>
2024-12-22 20:43:10 -0300

    Adicionando o projeto do HELLO WORLD

---
+++ mod5/PrimeiroProjeto/src/br/com/stpaes/PrimeiraClasse.java
@@ -0,0 +1,10 @@
+package br.com.stpaes;
+
+public class PrimeiraClasse {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Gustavo e EBAC!");
+		System.out.println("Iniciando minha jornada!");
+	}
+
+}
