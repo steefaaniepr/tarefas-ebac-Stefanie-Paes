47a9c1f165ce53ff26787f19c8d84b1280c6bed4
stpaes <steefaaniepr@outlook.com>
2024-12-22 21:10:16 -0300

    Atualizando os arquivos do projeto

--- mod5/PrimeiroProjeto/src/br/com/stpaes/PrimeiraClasse.java
+++ mod5/PrimeiroProjeto/src/br/com/stpaes/PrimeiraClasse.java
@@ -2,9 +2,9 @@ package br.com.stpaes;
 
 public class PrimeiraClasse {
 
-	public static void main(String[] args) {
-		System.out.println("Hello Gustavo e EBAC!");
-		System.out.println("Iniciando minha jornada!");
-	}
-
+    public static void main(String[] args) {
+        System.out.println("Hello Stefanie");
+        System.out.println("Hello Gustavo e EBAC!");
+        System.out.println("Testando o GIT e as suas camadas.");
+    }
 }
